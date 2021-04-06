e01df95a4cbbc3f8f4bd730e4401ef00ef5311c2
David Yoon <david.hyeshik@yahoo.com>
2021-04-06 10:59:52 +0900

    Update and rename lb.tf to 5.lb.tf

---
+++ 5.lb.tf
@@ -0,0 +1,9 @@
+resource "azurerm_lb" "david-lb" {
+  name = "davidlb"
+  location = azurerm_resource_group.david-rg.location
+  resource_group_name = azurerm_resource_group.david-rg.name
+  frontend_ip_configuration {
+  name = "davidPublicIPAddress"
+  public_ip_address_id = azurerm_public_ip.david-publicip.id
+ }
+}
--- lb.tf
+++
@@ -1,9 +0,0 @@
-resource "azurerm_lb" "david-lb" {
-name = "davidlb"
-location = azurerm_resource_group.david-rg.location
-resource_group_name = azurerm_resource_group.david-rg.name
-frontend_ip_configuration {
-  name = "davidPublicIPAddress"
-  public_ip_address_id = azurerm_public_ip.david-publicip.id
-}
-}
