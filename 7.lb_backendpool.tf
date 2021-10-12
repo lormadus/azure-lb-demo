e3fe25a4ca41defcd05c850b6a8359a56b501e61
David Yoon <david.hyeshik@yahoo.com>
2021-10-12 14:23:23 +0900

    Update 7.lb_backendpool.tf

--- 7.lb_backendpool.tf
+++ 7.lb_backendpool.tf
@@ -1,4 +1,4 @@
-resource "azurerm_lb_backend_address_pool" "david-bpepool" {
+resource "azurerm_lb_backend_address_pool" "david-bepool" {
     loadbalancer_id = azurerm_lb.david-lb.id
     name = "david-BackEndAddressPool"
 }
