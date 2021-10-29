1047edfd98c1ebb67edbb81bf89622fb1c03b60b
David Yoon <david.hyeshik@yahoo.com>
2021-10-29 13:43:41 +0900

    Update 5.public_ip.tf

--- 5.public_ip.tf
+++ 5.public_ip.tf
@@ -1,5 +1,5 @@
 resource "azurerm_public_ip" "david-publicip" {  
-name                = "mypublicIP"  
+name                = "davidmypublicip"  
 location            = azurerm_resource_group.david-rg.location  
 resource_group_name = azurerm_resource_group.david-rg.name  
 allocation_method   = "Static"  
