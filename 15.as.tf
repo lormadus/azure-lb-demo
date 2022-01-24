109fcaaa7e464a318ec961f62a72497d2f6bf1b5
David Yoon <david.hyeshik@yahoo.com>
2022-01-24 15:15:01 +0900

    Update 15.as.tf

--- 15.as.tf
+++ 15.as.tf
@@ -1,9 +1,8 @@
 resource "azurerm_availability_set" "avset" {
  name                         = "avset"
- location                     = azurerm_resource_group.david-rg.location
- resource_group_name          = azurerm_resource_group.david-rg.name
+ location                     = azurerm_resource_group.user20-rg.location
+ resource_group_name          = azurerm_resource_group.user20-rg.name
  platform_fault_domain_count  = 2
  platform_update_domain_count = 2
  managed                      = true
 }
-
