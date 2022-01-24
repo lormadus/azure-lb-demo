0990cec0defa8c6c9b5de4f4afa680d260345e31
David Yoon <david.hyeshik@yahoo.com>
2022-01-24 15:01:17 +0900

    Update 6.lb.tf

--- 6.lb.tf
+++ 6.lb.tf
@@ -1,9 +1,10 @@
-resource "azurerm_lb" "david-lb" {
-  name = "davidlb"
-  location = azurerm_resource_group.david-rg.location
-  resource_group_name = azurerm_resource_group.david-rg.name
+resource "azurerm_lb" "user20-lb" {
+  name                  = "user20-lb"
+  location              = azurerm_resource_group.user20-rg.location
+  resource_group_name   = azurerm_resource_group.user20-rg.name
+
   frontend_ip_configuration {
-  name = "davidPublicIPAddress"
-  public_ip_address_id = azurerm_public_ip.david-publicip.id
- }
+        name = "user20-pip"
+  public_ip_address_id = azurerm_public_ip.user20-publicip.id
+  }
 }
