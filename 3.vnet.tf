94c0b437346c3aaf65f068375fc32d47b7f69159
David Yoon <david.hyeshik@yahoo.com>
2022-01-24 14:59:29 +0900

    Update 3.vnet.tf

--- 3.vnet.tf
+++ 3.vnet.tf
@@ -1,6 +1,6 @@
-resource "azurerm_virtual_network" "david-net" {
-    name = "david-vnet"
-    address_space = ["40.0.0.0/16"]
-    location = azurerm_resource_group.david-rg.location
-    resource_group_name = azurerm_resource_group.david-rg.name
+resource "azurerm_virtual_network" "user20-net" {
+    name = "user20-vnet"
+    address_space = ["20.0.0.0/16"]
+    location = azurerm_resource_group.user20-rg.location
+    resource_group_name = azurerm_resource_group.user20-rg.name
 }
