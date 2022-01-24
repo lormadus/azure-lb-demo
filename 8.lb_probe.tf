1e930399fe072adf4792278728ddc3f6ad6e64d3
David Yoon <david.hyeshik@yahoo.com>
2022-01-24 15:02:17 +0900

    Update 8.lb_probe.tf

--- 8.lb_probe.tf
+++ 8.lb_probe.tf
@@ -1,8 +1,8 @@
-resource "azurerm_lb_probe" "david-lb-probe" {
-    resource_group_name = azurerm_resource_group.david-rg.name
-    loadbalancer_id = azurerm_lb.david-lb.id
-    name = "http-probe"
-    protocol = "Http"
-    request_path = "/"
-    port = 80
+resource "azurerm_lb_probe" "user20-lb-probe" {
+    resource_group_name = azurerm_resource_group.user20-rg.name
+    loadbalancer_id     = azurerm_lb.user20-lb.id
+    name                = "http-probe"
+    protocol            = "Http"
+    request_path        = "/"
+    port                = 80
 }
