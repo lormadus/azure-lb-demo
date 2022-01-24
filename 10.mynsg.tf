cf82b64f0db50bd4d552134fcbd53aa52ed71146
David Yoon <david.hyeshik@yahoo.com>
2022-01-24 15:00:43 +0900

    Update 10.mynsg.tf

--- 10.mynsg.tf
+++ 10.mynsg.tf
@@ -1,8 +1,8 @@
-resource "azurerm_network_security_group" "mynsg" {
-    name                = "myNetworkSecurityGroup"
-    location            = azurerm_resource_group.david-rg.location
-    resource_group_name = azurerm_resource_group.david-rg.name
-    
+resource "azurerm_network_security_group" "user20-nsg" {
+    name                = "user20-nsg"
+    location            = azurerm_resource_group.user20-rg.location
+    resource_group_name = azurerm_resource_group.user20-rg.name
+
     security_rule {
         name                       = "SSH"
         priority                   = 1001
@@ -11,7 +11,7 @@ resource "azurerm_network_security_group" "mynsg" {
         protocol                   = "Tcp"
         source_port_range          = "*"
         destination_port_range     = "22"
-        source_address_prefix      = "*"   ## Admin 서버 IP 주소 혹은 접속하는 서버(or PC) IP주소
+        source_address_prefix      = "211.45.60.5/32"
         destination_address_prefix = "*"
     }
  security_rule {
@@ -30,4 +30,3 @@ resource "azurerm_network_security_group" "mynsg" {
         environment = "Terraform Demo"
     }
 }
-
