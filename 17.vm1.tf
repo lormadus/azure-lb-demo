98144a62737e4464c087fcfb47cef6e29d85b0d9
David Yoon <david.hyeshik@yahoo.com>
2022-01-24 15:41:24 +0900

    Create 17.vm1.tf

--- 17.vm1.tf
+++ 17.vm1.tf
@@ -8,7 +8,7 @@ resource "azurerm_virtual_machine" "web1" {
     vm_size               = "Standard_DS1_v2"
 
     storage_os_disk {
-        name              = "myOsDisk"
+        name              = "myOsDisk1"
         caching           = "ReadWrite"
         create_option     = "FromImage"
         managed_disk_type = "Premium_LRS"
