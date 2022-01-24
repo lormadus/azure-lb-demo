687b7a408ac8e1a9e36aff4c3f11346ed80f358f
David Yoon <david.hyeshik@yahoo.com>
2022-01-24 15:27:39 +0900

    Update 17.vm1.tf

--- 17.vm1.tf
+++ 17.vm1.tf
@@ -1,7 +1,7 @@
 resource "azurerm_virtual_machine" "web1" {
-    name                  = "davidweb1"
-    location              = azurerm_resource_group.david-rg.location
-    resource_group_name   = azurerm_resource_group.david-rg.name
+    name                  = "user20web1"
+    location              = azurerm_resource_group.user20-rg.location
+    resource_group_name   = azurerm_resource_group.user20-rg.name
     availability_set_id   = azurerm_availability_set.avset.id
     delete_os_disk_on_termination    = true
     network_interface_ids = [azurerm_network_interface.nic1.id]
@@ -16,34 +16,30 @@ resource "azurerm_virtual_machine" "web1" {
     storage_image_reference {
         publisher = "Canonical"
         offer     = "UbuntuServer"
-        sku       = "16.04.0-LTS"
+        sku       = "18.04.0-LTS"
         version   = "latest"
     }
 
  os_profile {
-        computer_name  = "davidweb1"
+        computer_name  = "user20web1"
         admin_username = "azureuser"    ## 아래 34번 라인의 계정이름과 동일해야 함
         admin_password = "Pass****"     ## 12자리이상, 특수문자, 숫자, 대문자 조합으로 생성 필요
-	custom_data= file("web.sh")     ## Terraform 실행하는 서버에 존재해야 함, 실행은 만들어지는 VM에서
+        custom_data= file("web.sh")     ## Terraform 실행하는 서버에 존재해야 함, 실행은 만들어지는 VM에서
     }
 
  os_profile_linux_config {
         disable_password_authentication = false
         ssh_keys {
-	    ## ssh-keygen -t rsa -b 4096 -m PEM   명령어를 통해 Private Key(id_rsa)와 Public Key(id_rsa.pub)파일 생성
-            ## 서버 접근을 위해 관리 서버에서 생성한 id_rsa.pub 파일을 가상서버로 복사
-            path     = "/home/azureuser/.ssh/authorized_keys"   ## 가상 서버에 복사되는 위치
-	    ## id_rsa.pub 파일 내용을 아래 key_data에 넣어줌 (무조건 한줄로!!!)
+            path     = "/home/azureuser/.ssh/authorized_keys"
             key_data = file("~/.ssh/id_rsa.pub")
         }
     }
     boot_diagnostics {
         enabled     = "true"
-        storage_uri = azurerm_storage_account.mystorageaccount.primary_blob_endpoint
+        storage_uri = azurerm_storage_account.user20-diag-storage.primary_blob_endpoint
     }
 
     tags = {
         environment = "Terraform Demo"
     }
 }
-
