d7ac9644df1f4f394baf1bc7f83f4f968e4aba45
David Yoon <david.hyeshik@yahoo.com>
2022-01-24 15:03:36 +0900

    Update 12.natrule2.tf

--- 12.natrule2.tf
+++ 12.natrule2.tf
@@ -1,10 +1,9 @@
 resource "azurerm_lb_nat_rule" "natrule2" {
-  resource_group_name            = azurerm_resource_group.david-rg.name
-  loadbalancer_id                = azurerm_lb.david-lb.id
+  resource_group_name            = azurerm_resource_group.user20-rg.name
+  loadbalancer_id                = azurerm_lb.user20-lb.id
   name                           = "web2SSH"
   protocol                       = "Tcp"
   frontend_port                  = 50002
   backend_port                   = 22
-  frontend_ip_configuration_name = "davidPublicIPAddress"
+  frontend_ip_configuration_name = "user20-pip"
 }
-
