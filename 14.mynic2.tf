8ce71048f1df1dfbb04d0253ab6b02eff3e67bc7
David Yoon <david.hyeshik@yahoo.com>
2022-01-24 14:49:08 +0900

    Update 14.mynic2.tf

--- 14.mynic2.tf
+++ 14.mynic2.tf
@@ -1,13 +1,13 @@
-resource "azurerm_network_interface" "nic2" {
-    name                = "myNIC2"
-    location = azurerm_resource_group.david-rg.location
-    resource_group_name = azurerm_resource_group.david-rg.name
+resource "azurerm_network_interface" "web2-nic1" {
+    name                = "web2-nic1"
+    location = azurerm_resource_group.user20-rg.location
+    resource_group_name = azurerm_resource_group.user20-rg.name
 
     ip_configuration {
-        name                          = "myNicConfiguration2"
-        subnet_id                     = azurerm_subnet.david-subnet1.id
+        name                          = "web2-nic1-ip"
+        subnet_id                     = azurerm_subnet.user20-subnet1.id
         private_ip_address_allocation = "Dynamic"
-#        public_ip_address_id          = azurerm_public_ip.david-publicip.id
+#        public_ip_address_id          = azurerm_public_ip.user20-publicip.id
     }
 
     tags = {
@@ -16,18 +16,17 @@ resource "azurerm_network_interface" "nic2" {
 }
 
 resource "azurerm_network_interface_security_group_association" "nic-nsg-association2" {
-  network_interface_id      = azurerm_network_interface.nic2.id
-  network_security_group_id = azurerm_network_security_group.mynsg.id
+  network_interface_id      = azurerm_network_interface.web2-nic1.id
+  network_security_group_id = azurerm_network_security_group.user20-nsg.id
 }
 
 resource "azurerm_network_interface_backend_address_pool_association" "backendPool2" {
-  network_interface_id    = azurerm_network_interface.nic2.id
-  ip_configuration_name   = "myNicConfiguration2"
-  backend_address_pool_id = azurerm_lb_backend_address_pool.david-bpepool.id 
+  network_interface_id    = azurerm_network_interface.web2-nic1.id
+  ip_configuration_name   = "web2-nic1-ip"
+  backend_address_pool_id = azurerm_lb_backend_address_pool.user20-bep.id
 }
-
 resource "azurerm_network_interface_nat_rule_association" "natrule2" {
-  network_interface_id  = azurerm_network_interface.nic2.id
-  ip_configuration_name = "myNicConfiguration2"
-  nat_rule_id           = azurerm_lb_nat_rule.natrule2.id
+  network_interface_id  = azurerm_network_interface.web2-nic1.id
+  ip_configuration_name = "web2-nic1-ip"
+  nat_rule_id           = azurerm_lb_nat_rule.natrule1.id
 }
